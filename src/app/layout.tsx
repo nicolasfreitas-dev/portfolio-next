7ec835346b2178ae01ed10a308fa537bb74fccc1
Nicolas <nicolasfpdev@gmail.com>
2025-03-21 03:46:25 -0300

    chore: add metadata for SEO on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Nicolas Freitas",
+  description: "Portfolio pessoal para demonstrar meus projetos e habilidades como Desenvolvedor Fullstack",
+}
 
 export default function RootLayout({
   children,
