b1549669680f57ce3a163a6d05c6030f2e890669
Nicolas <nicolasfpdev@gmail.com>
2025-03-21 12:54:15 -0300

    feat: create Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,65 @@
+import { Mail } from "lucide-react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import linkedin from "@/assets/linkedin.svg"
+import github from "@/assets/github.svg"
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer>
-            
+        <footer className="border-t py-8 md:py-12">
+            <div className="container px-4 md:px-6">
+                <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                    <span className="font-bold text-xl">Nicolas Freitas</span>
+                    <p className="text-mutedForeground text-sm max-w-[500px]">
+                        Desenvolvedor Fullstack especializado em construir
+                        soluções digitais que fazem a diferença. Vamos trabalhar
+                        juntos para trazer suas ideias a vida.
+                    </p>
+                    <div className="flex items-center gap-5">
+                        <Button variant="ghost" size="icon">
+                            <Link
+                                href="https://github.com/nicolasfreitas-dev"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label="GitHub"
+                            >
+                                <Image
+                                    src={github}
+                                    alt="github icon"
+                                    width={20}
+                                    height={20}
+                                />
+                            </Link>
+                        </Button>
+                        <Button variant="ghost" size="icon">
+                            <Link
+                                href="https://www.linkedin.com/in/nicolasfreitas-dev/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Image
+                                    src={linkedin}
+                                    alt="linkedin icon"
+                                    width={20}
+                                    height={20}
+                                />
+                            </Link>
+                        </Button>
+                        <Button variant="ghost" size="icon">
+                            <Link href="">
+                                <Mail className="h-5 w-5" />
+                            </Link>
+                        </Button>
+                    </div>
+                    <div>
+                        <span className="text-sm text-mutedForeground">
+                            © {new Date().getFullYear()} Nicolas Freitas. Todos
+                            os direitos reservados.
+                        </span>
+                    </div>
+                </div>
+            </div>
         </footer>
-    )
+    );
 }
