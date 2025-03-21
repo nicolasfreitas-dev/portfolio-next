7bf4f55e661d019c89804bc3f39665ce326ea724
Nicolas <nicolasfpdev@gmail.com>
2025-03-21 03:47:32 -0300

    feat: start creation of Footer, Projects, Contact component and add do App.tsx to visualize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-      <main>
+      <div className="min-h-screen bg-secondary">
         <Header />
-        
-      </main>
+        <main>
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
   );
 }
