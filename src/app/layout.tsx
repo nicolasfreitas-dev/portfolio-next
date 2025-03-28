0c469dfa1f64460270d0d1c96f6aa216fc16c6c9
Nicolas <nicolasfpdev@gmail.com>
2025-03-28 12:09:26 -0300

    chore: add SEO improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Nicolas Freitas",
-  description: "Portfolio pessoal para demonstrar meus projetos e habilidades como Desenvolvedor Fullstack.",
+  title: "Portfólio | Nicolas Freitas - Desenvolvedor Fullstack",
+  description: "Desenvolvedor Fullstack com experiência em React, Javascript, Typescript, Next.js, Node.js e Java. Especializado em construir soluções digitais que fazem a diferença. Atuo no desenvolvimento e manutenção de aplicações web completas, na criação de sites, interfaces e APIs.",
   icons: {
     icon: "/favicon.ico"
-  }
+  },
+  keywords: ["Portfólio", "React", "NextJS", "Typescript", "Javascript", "Desenvolvedor Fullstack", "GIT", "NodeJS", "HTML", "CSS", "Tailwindcss", "Java", "MySQL", "PostgreSQL", "Prisma", "Express", "Axios", "Jest", "Cypress", "Testes automatizados", "Testes unitários"]
 }
 
 export default function RootLayout({
