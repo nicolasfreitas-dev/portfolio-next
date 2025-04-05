a2b029a287b531c458cb82466f381d9687af890a
nicolasfreitas <nicolasfpdev@gmail.com>
2025-04-05 11:25:53 -0300

    feat: add new contact informations and hyperlinks

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,8 @@
-import { Mail, Phone, MapPin } from "lucide-react";
+import { Mail, Phone } from "lucide-react";
 import { Card, CardContent } from "../ui/card";
+import linkedin from "@/assets/linkedin.svg"
+import github from "@/assets/github.svg"
+import Image from "next/image";
 
 export default function Contact() {
     return (
@@ -11,8 +14,8 @@ export default function Contact() {
                     </h2>
                     <div className="w-12 h-1 bg-primary rounded-full"></div>
                     <p className="max-w-[700px] text-muted-foreground md:text-lg">
-                        Tem um projeto em mente ou gostaria de discutir uma potencial
-                        oportunidade? Sinta-se livre para me contatar.
+                        Tem um projeto em mente ou gostaria de discutir uma
+                        potencial oportunidade? Sinta-se livre para me contatar.
                     </p>
                 </div>
                 <div className="max-w-[720px] flex flex-col gap-6 items-center justify-center mx-auto">
@@ -45,12 +48,43 @@ export default function Contact() {
                     <Card className="w-full">
                         <CardContent className="p-6 flex items-center gap-4">
                             <div className="bg-primary/10 p-3 rounded-full">
-                                <MapPin className="h-6 w-6 text-primary" />
+                                <Image
+                                    src={linkedin}
+                                    className="h-6 w-6 text-primary"
+                                    alt="linkedin icon"
+                                />
                             </div>
                             <div>
-                                <h3 className="font-bold">Localização</h3>
+                                <h3 className="font-bold">Linkedin</h3>
                                 <p className="text-muted-foreground">
-                                    Fortaleza, Ceará, Brasil
+                                    <a
+                                        href="https://www.linkedin.com/in/nicolasfreitas-dev/"
+                                        target="_blank"
+                                    >
+                                        https://www.linkedin.com/in/nicolasfreitas-dev/
+                                    </a>
+                                </p>
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card className="w-full">
+                        <CardContent className="p-6 flex items-center gap-4">
+                            <div className="bg-primary/10 p-3 rounded-full">
+                                <Image
+                                    src={github}
+                                    className="h-6 w-6 text-primary"
+                                    alt="github icon"
+                                />
+                            </div>
+                            <div>
+                                <h3 className="font-bold">Github</h3>
+                                <p className="text-muted-foreground">
+                                    <a
+                                        href="https://github.com/nicolasfreitas-dev"
+                                        target="_blank"
+                                    >
+                                        https://github.com/nicolasfreitas-dev
+                                    </a>
                                 </p>
                             </div>
                         </CardContent>
